d4fc34879f578501c13f22b33d8dfe2fda412496
nijuyonkadesu <24ksriramv@gmail.com>
2023-02-26 01:18:53 +0530

    implementation of a|b

--- regex/nfa.cpp
+++ regex/nfa.cpp
@@ -15,14 +15,9 @@ NFA::NFA(unsigned size_, state initial_, state final_){
 
     // Initialize trans_table with an "empty graph"
     // cannot pre-determine the size of matrix, hence have to push back manually
-    for(unsigned i = 0; i < size; ++i){
-        vector<state> v;
-        for(unsigned j = 0; j < size; ++j){
-            v.push_back(NONE);
-        }
+    vector<input> v(size, NONE);
+    for(unsigned i = 0; i < size; ++i)
         trans_table.push_back(v);
-        v.clear();
-    }
 }
 
 bool NFA::is_legal_state(state s){
@@ -35,6 +30,81 @@ void NFA::add_trans(state from, state to, input in){
     assert(is_legal_state(to));
     trans_table[from][to] = in;
 }
+
+void NFA::append_empty_state(){
+
+    // append an empty row at the bottom 
+    // with size + 1 as length
+    vector<input> empty_row(size+1, NONE);
+    trans_table.push_back(empty_row);
+    
+    // append a column at the right
+    for(state i = 0; i < size; ++i)
+        trans_table[i].push_back(NONE);
+    size += 1;
+}
+
+void NFA::shift_states(unsigned shift){
+    // calculate new size and create a new transition table 
+    unsigned new_size = size + shift;
+    vector<input> empty_row(new_size, NONE);
+    vector<vector<input>> new_trans_table(new_size, empty_row);
+
+    for(state i = 0; i < size; ++i)
+        for(state j = 0; j < size; ++j)
+            new_trans_table[i + shift][j + shift] = trans_table[i][j];
+
+    // update NFA members
+    size = new_size;
+    initial += shift;
+    final += shift;
+    trans_table = new_trans_table;
+}
+
+void NFA::fill_states(const NFA &other){
+
+    // trans_table = other.trans_table;
+    for (state i = 0; i < other.size; ++i){
+        for (state j = 0; j < other.size; ++j){
+            trans_table[i][j] = other.trans_table[i][j];
+        }
+    }
+
+    // copying inputs of other to nfa2
+    for(set<input>::const_iterator i = other.inputs.begin();
+            i != other.inputs.end(); ++i)
+        inputs.insert(*i);
+}
+
+NFA build_nfa_alter(NFA nfa1, NFA nfa2){
+    // Make room for new initial state
+    // #1
+    nfa1.shift_states(1);
+
+    // Make room for nfa1 to be combined
+    // #2
+    nfa2.shift_states(nfa1.size);
+
+    // copy of nfa2
+    NFA new_nfa(nfa2);
+
+    // #1 + #2, ie, combine trans_table
+    new_nfa.fill_states(nfa1);
+
+    // new initial state
+    new_nfa.initial = 0;
+    new_nfa.add_trans(0, nfa1.initial, EPS);
+    new_nfa.add_trans(0, nfa2.initial, EPS);
+    
+    // new final state
+    new_nfa.append_empty_state();
+    new_nfa.final = new_nfa.size - 1;
+    new_nfa.add_trans(nfa1.final, new_nfa.final, EPS);
+    new_nfa.add_trans(nfa2.final, new_nfa.final, EPS);
+
+    return new_nfa;
+}
+
 
 void NFA::show() {
     cout<<"NFA States: "<<size<<" 0 to "<<size-1<<endl;
--- regex/nfa.h
+++ regex/nfa.h
@@ -1,6 +1,7 @@
 #ifndef NFA_H
 #define NFA_H
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -15,15 +16,25 @@ class NFA {
 
         // add a new transtion from a state to another with a input
         void add_trans(state from, state to, input in);
-
         void show(void);
 
-    private:
+        void append_empty_state();
+        void shift_states(unsigned shift);
+        void fill_states(const NFA& other);
+
+    // private:
         bool is_legal_state(state s);
         state initial;
         state final;
         unsigned size;
-        vector<vector<state>> trans_table;
+        vector<vector<input>> trans_table;
+
+        // set of all input states
+        set<input> inputs;
 };
+
+// NFA building functions
+
+NFA build_nfa_alter(NFA nfa1, NFA nfa2);
 
 #endif
