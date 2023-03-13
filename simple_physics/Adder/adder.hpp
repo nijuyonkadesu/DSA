4a549126a121ae30f9fed14d7aa3f806b5d7669d
nijuyonkadesu <24ksriramv@gmail.com>
2023-03-13 22:54:16 +0530

    CMakeLists project def, basic lib usage

---
+++ simple_physics/Adder/adder.hpp
@@ -0,0 +1,6 @@
+#ifndef ADDER_H
+#define ADDER_H
+
+float add(float a, float b);
+
+#endif // DEBUG
---
+++ simple_physics/main.cpp
@@ -0,0 +1,8 @@
+#include"Adder/adder.hpp"
+#include<iostream>
+
+int main(){
+    std::cout<<"This is makecake";
+    std::cout<<add(8.8f, 0.8f);
+    return 0;
+}
